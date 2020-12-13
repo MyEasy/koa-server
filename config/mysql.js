c4b6a30364f0f68433a3136dbc1f0a1228a87019
赵程委 <zhaochengwei@zhaochengweideMacBook-Pro.local>
2020-12-13 09:46:46 +0800

    msg:init project

---
+++ config/mysql.js
@@ -0,0 +1,9 @@
+const config = {
+  database: 'koa_server',
+  username: 'root',
+  password: '12345678',
+  host: 'localhost',
+  port: 3306
+};
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,61 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const { Sequelize, DataTypes } = require('sequelize');
+const config = require('./config/mysql');
+
+const app = new Koa();
+const router = new Router();
+
+const sequelizeSql = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
+    dialect: 'mysql',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 30000
+    }
+  }
+)
+
+const User = sequelizeSql.define('User',
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4
+    },
+    user_name: DataTypes.STRING,
+    login_name: DataTypes.STRING,
+    login_password: DataTypes.STRING,
+  },
+  {
+    tableName: 'koa_user',
+    timestamps: false
+  },
+);
+
+(async () => {
+  await sequelizeSql.sync({ force: true });
+  // 这里是代码
+  // 创建一个新用户
+  const user = await User.create({ user_name: 'Jane', login_name: 'Doe', login_password: '123' });
+  console.log("Jane's auto-generated ID:", user.id);
+})();
+
+router.post('/gateway/user/login', (ctx) => {
+  console.log(ctx.request);
+  ctx.body = 'login success';
+  console.log('success')
+})
+
+app.use(bodyParser());
+app.use(router.routes());
+
+app.listen(3000, () => {
+  console.log('app is running on 3000 port')
+});
